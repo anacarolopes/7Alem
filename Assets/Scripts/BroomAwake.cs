102847ece84d8c3a88d19c1ee1f238e63231fb2d
anacarolopes <38691476+anacarolopes@users.noreply.github.com>
2020-10-12 16:34:30 -0300

    vassouras
    acionamento das vassouras para acesso a porta

---
+++ Assets/Scripts/BroomAwake.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroomAwake : MonoBehaviour
+{
+   private bool moveDown = true;
+    public float velocidade =3f;
+    public Transform pontoA;
+    public Transform pontoB;
+
+    void Update()
+    {
+        if (ActBroom.acionouBroom == true)
+        {
+            if (transform.position.x < pontoA.position.x)
+            moveDown = true;
+            if (transform.position.x > pontoB.position.x)
+            moveDown = false;
+        
+            if(moveDown)
+            transform.position = new Vector2(transform.position.x + velocidade * Time.deltaTime, transform.position.y);
+            else
+            transform.position = new Vector2(transform.position.x - velocidade * Time.deltaTime, transform.position.y);
+        }
+    }
+}
